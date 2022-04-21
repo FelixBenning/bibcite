09a716624eb7eb916fd8706b81c024529f6ab143
Felix Benning <felix.benning@gmail.com>
2022-04-21 13:48:44 +0200

    save unsaved

--- bibcite/index.ts
+++ bibcite/index.ts
@@ -1,7 +1,7 @@
 import { BibController } from './bibController';
 import { Citation } from './citation';
-import { References } from './references';
+import { BibReference } from './bibReference';
 
 customElements.define('bib-config', BibController);
 customElements.define('bib-cite', Citation);
-customElements.define('bib-references', References);
+customElements.define('bib-references', BibReference);
