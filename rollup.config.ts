0708a197e3a5eda8e6d426d8a4f3bf19be00e614
Felix Benning <felix.benning@gmail.com>
2022-04-28 20:49:23 +0200

    remove prod config

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,23 +4,34 @@ import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import nodePolyfills from "rollup-plugin-polyfill-node";
 import json from "@rollup/plugin-json";
+import { terser } from "rollup-plugin-terser";
+
+const default_plugins = [
+  nodeResolve({ browser: true }),
+  commonjs({
+    include: "node_modules/**",
+  }),
+  typescript(),
+  nodePolyfills(),
+  json(),
+];
 
 const config = defineConfig({
   input: "bibcite/index.ts",
-  output: {
-    file: "dist/bibcite.js",
-    format: "cjs",
-  },
-  plugins: [
-    nodeResolve({browser: true}),
-    commonjs({
-      include: "node_modules/**",
-      // dynamicRequireTargets: ["node_modules/citation-js"],
-    }),
-    typescript(),
-    nodePolyfills(),
-    json(),
+  output: [
+    {
+      file: "dist/bibcite.js",
+      format: "cjs",
+    },
+    {
+      file: "dist/bibcite.min.js",
+      format: "cjs",
+      plugins: [
+        terser({format: {comments: false}})
+      ],
+    },
   ],
+  plugins: default_plugins,
 });
 
 export default config;
